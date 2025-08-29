92eb0b6ad5ff9f392ba70ae72da065620da1e3e3
SHREYANSH SINHA <sinha.14shreyansh@gmail.com>
2025-08-30 01:20:07 +0530

    new changes

---
+++ src/index.js
@@ -0,0 +1,11 @@
+//quire('dotenv').config({path: './env'})
+
+
+
+
+//port mongoose from "mongoose";
+//port { DB_NAME } from "./constants.js";
+import connectDB from "./db/index.js";
+
+
+connectDB()
