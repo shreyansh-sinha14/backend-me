e584ce009ff828dbd82da80a8514441393294d59
SHREYANSH SINHA <sinha.14shreyansh@gmail.com>
2025-08-31 01:09:48 +0530

    util code discussion

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"// user ke browser se cookies access karne ke liye and set karne ke liye
+
+
+const app = express()
+app.use(cors({
+    origin : process.env.CORS_ORIGIN,
+    credentials : true
+}))
+
+app.use(express.json({limit: "16kb"})) // configuration for json 
+app.use(express.urlencoded({extended: true, limit : "16kb"})) //configuration url based data
+app.use(express.static("public")) // static folder for images and pdfs
+app.use(cookieParser()) // cookie parser middleware
+
+export{app}
