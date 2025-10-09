0e7656decfdcc3374cff103ffbf1772e28d25693
SHREYANSH SINHA <sinha.14shreyansh@gmail.com>
2025-10-09 23:51:29 +0530

    Add models for comments, likes, playlists, and tweets with schemas and timestamps

---
+++ src/models/comment.model.js
@@ -0,0 +1,25 @@
+import mongoose, { Schema } from 'mongoose';
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const commentSchema = new Schema(
+    {
+        content : {
+            type : String,
+            required : true
+        },
+        video : {
+            type : Schema.Types.ObjectId,
+            ref : "Video",
+            required : true
+        },
+        owner : {
+            type : Schema.Types.ObjectId,
+        }
+    },
+        {
+            timestamps : true
+        }
+)
+
+videoSchema.plugin(mongooseAggregatePaginate);
+export const Comment = mongoose.model("Comment", commentSchema)
